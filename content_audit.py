438109bfc01d25e098e8ff5b807539eb2c67dfc0
Shawn Borton <shawn@design-centric.com>
2012-09-14 12:16:03 -0500

    Updated user agent
    Updated user agent string to reflect Firefox v15 on Mac OS X v10.6

--- content_audit.py
+++ content_audit.py
@@ -18,7 +18,7 @@ class ContentAuditor:
     This script takes a list of URLs and retrieves data such as meta tags
     containing keywords, description and the page title. It then populates a
     spreadsheet with the data for easy review.
-    """ 
+    """
 
     def __init__(self, filename):
         """
@@ -28,13 +28,13 @@ class ContentAuditor:
         BeautifulSoup for HTML parsing
         xlwt for writing to an Excel spreadsheet without use of COM Interop
         """
-        self.filehandle = open(filename, 'r')  
+        self.filehandle = open(filename, 'r')
         self.soupy_data = ""
         self.workbook = ""
         self.current_sheet = ""
         self.content = ""
         self.text_file = ""
-        self.site_info = [] 
+        self.site_info = []
         self.url_parts = ""
         self.reg_expres = re.compile(r"www.(.+?)(.com|.net|.org)")
 
@@ -43,7 +43,7 @@ class ContentAuditor:
         read_url
 
         Method which reads in a given url (to the constructor) and puts data
-        into a BeautifulSoup context. 
+        into a BeautifulSoup context.
 
         We begin setting a string for the user-agent. Checking for comment
         lines in the URL list, we take a web address, one at a time, download
@@ -51,20 +51,20 @@ class ContentAuditor:
         Along the way, we check for any connectivity or remote server issues
         and handle them appropriately.
         """
-        ua_string = 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.4; en-US; rv:1.9.0.9) Gecko/2009040820 Firefox/3.0.9' 
+        ua_string = 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.6; en-US; rv:1.9.0.9) Gecko/20120716 Firefox/15.0a2'
         for line in self.filehandle:
             if line.startswith("#"):
                 continue
-	    print "Parsing %s" % line 
+	        print "Parsing %s" % line
             self.url_parts = urlparse.urlparse(line)
             req = urllib2.Request(line)
             req.add_header('User-Agent', ua_string)
             try:
-                data = urllib2.urlopen(req) 
+                data = urllib2.urlopen(req)
             except urllib2.HTTPError, ex:
                 print "Could not parse %s. The server returned the following:" % line
                 print "Error code: ", ex.code
-                print "Moving on to the next one..." 
+                print "Moving on to the next one..."
                 continue
             except urllib2.URLError, urlex:
                 print "Could not parse %s. We did not reach a server." % line
@@ -77,7 +77,7 @@ class ContentAuditor:
 
     #Extraction methods
 
-    def extract_tags(self): 
+    def extract_tags(self):
         """
         extract_tags
 
@@ -87,12 +87,12 @@ class ContentAuditor:
         page_info = {}
 
         for tag in self.soupy_data.findAll('meta', attrs={"name": True}):
-            page_info[tag['name']] = tag['content'] 
+            page_info[tag['name']] = tag['content']
         page_info['title'] = self.soupy_data.head.title.contents[0]
-        page_info['filename'] = self.url_parts[2] 
+        page_info['filename'] = self.url_parts[2]
         self.add_necessary_tags(page_info, ['keywords', 'description', 'title'])
         self.site_info.append(page_info)
-        self.soupy_data = "" 
+        self.soupy_data = ""
 
     def extract_text(self):
         """
@@ -105,7 +105,7 @@ class ContentAuditor:
         self.text_file.write(self.content)
         self.text_file.close()
 
-    #Spreadsheet methods 
+    #Spreadsheet methods
 
     def write_to_spreadsheet(self):
         """
@@ -115,10 +115,10 @@ class ContentAuditor:
         url
         """
         self.workbook = xlwt.Workbook()
- 
-        page_name = self.reg_expres.match(self.url_parts[1]) 
+
+        page_name = self.reg_expres.match(self.url_parts[1])
         self.current_sheet = self.workbook.add_sheet(page_name.group(1))
- 
+
         self.current_sheet.write(0, 0, "Page Name")
         self.current_sheet.write(0, 1, "File Name")
         self.current_sheet.write(0, 2, "Page Title")
@@ -132,9 +132,9 @@ class ContentAuditor:
             self.current_sheet.write(count, 1, dex['filename'])
             self.current_sheet.write(count, 2, dex['title'])
             self.current_sheet.write(count, 3, dex['description'])
-            self.current_sheet.write(count, 4, dex['keywords']) 
+            self.current_sheet.write(count, 4, dex['keywords'])
             count += 1
-        
+
         #self.workbook.save('content_audit.xls')
         self.workbook.save(options.output)
 
@@ -159,16 +159,16 @@ if __name__ == "__main__":
     parser.add_option("-f", "--file", dest="filename",
                       help="Filename containing URLs", metavar="FILE")
     parser.add_option("-o", "--output", dest="output",
-                      help="Output file, usually a spreadsheet") 
+                      help="Output file, usually a spreadsheet")
 
     (options, args) = parser.parse_args()
 
     if not options.output:
         parser.error("You did not specify an output file.")
-        
+
     if options.filename:
         content_bot = ContentAuditor(options.filename)
         content_bot.read_url()
         content_bot.write_to_spreadsheet()
     else:
-        parser.error("You did not specify an input file (a list of URLs)") 
+        parser.error("You did not specify an input file (a list of URLs)")
